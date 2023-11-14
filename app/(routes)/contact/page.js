aa675b2a9775a4c13fd26d6e217c03f65cca6141
Julian <cusco2891@gmail.com>
2023-11-14 15:48:58 -0500

    form update

--- app/(routes)/contact/page.js
+++ app/(routes)/contact/page.js
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Box, Container, Grid, Typography, TextField } from '@mui/material'
 import Image from 'next/image'
 import styles from './contact.module.css';
@@ -14,8 +14,44 @@ export default function page() {
   const [email, setEmail] = useState(false);
 
 
-
-
+  useEffect(() => {
+    const isNumericInput = (event) => {
+      const key = event.keyCode;
+      return ((key >= 48 && key <= 57) || // Allow number line
+        (key >= 96 && key <= 105) // Allow number pad
+      );
+    };
+    const isModifierKey = (event) => {
+      const key = event.keyCode;
+      return (event.shiftKey === true || key === 35 || key === 36) || // Allow Shift, Home, End
+        (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
+        (key > 36 && key < 41) || // Allow left, up, right, down
+        (
+          // Allow Ctrl/Command + A,C,V,X,Z
+          (event.ctrlKey === true || event.metaKey === true) &&
+          (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
+        )
+    };
+    const enforceFormat = (event) => {
+      // Input must be of a valid number format or a modifier key, and not longer than ten digits
+      if (!isNumericInput(event) && !isModifierKey(event)) {
+        event.preventDefault();
+      }
+    };
+    const formatToPhone = (event) => {
+      if (isModifierKey(event)) { return; }
+      const input = event.target.value.replace(/\D/g, '').substring(0, 10); // First ten digits of input only
+      const areaCode = input.substring(0, 3);
+      const middle = input.substring(3, 6);
+      const last = input.substring(6, 10);
+      if (input.length > 6) { event.target.value = `(${areaCode}) ${middle} - ${last}`; }
+      else if (input.length > 3) { event.target.value = `(${areaCode}) ${middle}`; }
+      else if (input.length > 0) { event.target.value = `(${areaCode}`; }
+    };
+    const inputElement = document.getElementById('phone');
+    inputElement.addEventListener('keydown', enforceFormat);
+    inputElement.addEventListener('keyup', formatToPhone);
+  }, [])
 
 
   async function handleSubmit(event) {
@@ -38,24 +74,16 @@ export default function page() {
       }
 
       const responseData = await response.json();
-      console.log(responseData['message'])
-      alert('Message successfully sent');
-
-
+      // console.log(responseData['message'])
+      // alert('Message successfully sent');
 
 
 
     } catch (error) {
       console.error(error);
-      alert("Error, please try resubmitting the form");
+      // alert("Error, please try resubmitting the form");
     }
-
   };
-
-
-
-
-
 
 
 
@@ -63,22 +91,13 @@ export default function page() {
     <Container maxWidth="1" style={{ backgroundColor: '', margin: "auto", backgroundColor: '', marginTop: '20vh' }}>
 
       <Grid container maxWidth='lg' style={{ margin: 'auto', backgroundColor: '', padding: '40px' }}>
-
-
-
         <Grid item md={12} style={{ backgroundColor: '', textAlign: 'center' }}>
           <Typography variant='h5' pb={4} gutterBottom style={{ fontWeight: "600", color: '#40A5FE' }}>
             Get in Touch with Mama's Cleaning Service - Your Trusted Cleaning Partner
           </Typography>
-          <Typography variant='p' style={{ fontSize: '1.4em' }}>
-            Let's make your space sparkling clean!
-          </Typography>
         </Grid>
-
-
-
-
       </Grid>
+
       <Grid container maxWidth='1' style={{ margin: 'auto', backgroundColor: '', height: '50vh', paddingBottom: '500px' }}>
 
         <Grid item md={6} style={{ backgroundColor: '', position: 'relative' }}>
@@ -88,17 +107,23 @@ export default function page() {
             fill
             position='absolute'
             style={{ objectFit: 'contain' }}
+            sizes='(max-width: 768px) 100vw, 33vw'
           />
         </Grid>
+
         <Grid item xs={12} md={6} style={{ backgroundColor: '', position: 'relative' }}>
 
-          <Box className={styles.info}>
 
+
+
+          <Box className={styles.info}>
             <Typography><a style={{ textDecoration: 'none' }} href="mailto:mamascleaningnj1@gmail.com">mamascleaningnj1@gmail.com</a></Typography>
             <Typography px={4}>|</Typography>
             <Typography><a style={{ textDecoration: 'none' }} href="tel:9083706943">(908) 370-6943</a></Typography>
-
           </Box>
+
+
+
           <Box className={styles.infosmall} py={2}>
 
             <Typography><a style={{ textDecoration: 'none' }} href="mailto:mamascleaningnj1@gmail.com">mamascleaningnj1@gmail.com</a></Typography>
@@ -108,91 +133,83 @@ export default function page() {
 
 
 
-
-
-          <Box
-            component="form"
-            sx={{
-              '& .MuiTextField-root': { m: 1, width: '25ch' },
-              bgcolor:''
-            }}
-            noValidate
-            autoComplete="off"
-            onSubmit={handleSubmit}
-          >
-
-
-            <div style={{ display: 'flex' }}>
-              <TextField
-                required
-                // error
-                id="fname"
-                name="fname"
-                label="First Name"
-              // defaultValue="Hello World"
-              />
-
-              <TextField
-                required
-                id="lname"
-                name="lname"
-                label="Last Name"
-              />
-            </div>
+          {/* Form */}
+          <div style={{ backgroundColor: '' }}>
 
 
 
-            <div style={{ display: 'flex' }}>
-              <TextField
-                required
-                id="email"
-                name="email"
-                type='email'
-                label="Email"
-              />
-
-              <TextField
-                required
-                id="phone"
-                label="Phone"
-                name="phone"
-
-              />
-            </div>
+            <form
+              onSubmit={handleSubmit}
+              className={styles.formClass}
+            >
 
 
-            <div>
-              <TextField
-                id="message"
-                label="Message"
-                name="message"
-                multiline
-                rows={4}
-              // defaultValue="Default Value"
-              />
-            </div>
+              <div style={{ display: 'flex' }}>
+                <input
+                  required
+                  id="fname"
+                  name="fname"
+                  placeholder='First Name'
+                  type='text'
+                />
+                <input
+                  required
+                  id="lname"
+                  name="lname"
+                  placeholder='Last Name'
+                  type='text'
+
+                />
+              </div>
 
 
 
+              <div style={{ display: 'flex' }}>
+                <input
+                  required
+                  id="email"
+                  name="email"
+                  type='email'
+                  placeholder='Email'
+                />
+                <input
+                  required
+                  id="phone"
+                  name="phone"
+                  placeholder='(123) 456 - 7890'
+                  type='tel'
+                  maxLength={16}
+                  pattern="\([0-9]{3}\)? ?[0-9]{3}? ?-? ?[0-9]{4}"
+                />
+              </div>
 
 
-            <div className={email ? styles.hiddenClass : styles.sendEmailBtn}>
-              <Button type='submit' variant="contained">Send</Button>
-            </div>
-
-
-            <div className={ email ? styles.sendMailMessage : styles.hiddenClass}>
-            <Typography>Email sent! Thank you!</Typography>
-            </div>
-
-
+              <div style={{ paddingLeft: '10px', paddingRight: '10px' }}>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder='Message..'
+                />
+              </div>
 
 
 
-          </Box>
+              <div className={email ? styles.hiddenClass : styles.sendEmailBtn}>
+                <Button type='submit' variant="contained">Send</Button>
+              </div>
+
+
+              <div className={email ? styles.sendMailMessage : styles.hiddenClass}>
+                <Typography>Email sent! Thank you!</Typography>
+              </div>
 
 
 
+            </form>
+
+
+
+          </div>
 
 
 
